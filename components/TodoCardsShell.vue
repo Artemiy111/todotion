bc343284891ff95b58f3279c6b90b6777cdfe47b
Artemiy111 <mrart111@mail.ru>
2023-02-05 20:05:10 +0500

    feat: add sorting to cards

--- components/TodoCardsShell.vue
+++ components/TodoCardsShell.vue
@@ -13,42 +13,60 @@
           @pick-color="pickColor" /></AppPopup
     ></Teleport>
 
-    <TodoCardCreate placeholder="Новая карточка" @create="createCard($event)" />
-    <template v-if="store.cards.length">
-      <Draggable
-        class="flex flex-col gap-3"
-        :list="cardsSortedByOrder"
-        tag="div"
-        item-key="id"
-        :animation="100"
-        handle=".drag-handler"
-        @change="changeCardOrder"
+    <div class="mb-4 flex flex-wrap items-center gap-2">
+      <span class="px-3">Сортировать по</span>
+      <select
+        v-model="orderBy"
+        title="order by"
+        class="w-full cursor-pointer appearance-none rounded-xl bg-slate-50 py-2 px-3 outline-none hover:bg-slate-100 focus:bg-slate-100 dark:bg-slate-800 dark:hover:bg-slate-700 dark:focus:bg-slate-700 md:w-auto"
       >
-        <template #item="{ element: card }">
-          <TodoCard
-            :id="card.id"
-            :key="card.id"
-            :title="card.title"
-            :color="card.color"
-            :is-selected="isSelected(card.id).value"
-            @select="selectCard(card.id)"
-            @update="updateCard(card.id, $event)"
-            @delete="deleteCard(card.id)"
-            @open-color-picker="openColorPicker"
-          >
-            <template #drag-handler>
-              <img
-                width="24"
-                height="24"
-                src="~/assets/drag.png"
-                alt=""
-                class="drag-handler h-6 cursor-grab [user-select:none] dark:invert"
-              />
-            </template>
-          </TodoCard>
-        </template>
-      </Draggable>
-    </template>
+        <option value="order">порядку</option>
+
+        <option value="time create new">созданию | от новых</option>
+        <option value="time create old">созданию | от старых</option>
+
+        <option value="time update new">обновлению | от новых</option>
+        <option value="time update old">обновлению | от старых</option>
+      </select>
+    </div>
+
+    <TodoCardCreate placeholder="Новая карточка" @create="createCard($event)" />
+
+    <Draggable
+      v-if="store.cards.length"
+      class="flex flex-col gap-3"
+      :list="sortedCards"
+      tag="div"
+      item-key="id"
+      :animation="100"
+      handle=".drag-handler"
+      @change="changeCardOrder"
+    >
+      <template #item="{ element: card }">
+        <TodoCard
+          :id="card.id"
+          :key="card.id"
+          :title="card.title"
+          :color="card.color"
+          :is-selected="isSelected(card.id).value"
+          @select="selectCard(card.id)"
+          @update="updateCard(card.id, $event)"
+          @delete="deleteCard(card.id)"
+          @open-color-picker="openColorPicker"
+        >
+          <template #drag-handler>
+            <img
+              width="24"
+              height="24"
+              src="~/assets/drag.png"
+              alt=""
+              class="h-6 cursor-grab [user-select:none] dark:invert"
+              :class="orderBy === 'order' ? 'drag-handler' : 'hidden'"
+            />
+          </template>
+        </TodoCard>
+      </template>
+    </Draggable>
   </div>
 </template>
 
@@ -74,6 +92,40 @@ const emit = defineEmits<{
 onMounted(() => {
   store.getAll()
 })
+
+type OrderBy =
+  | 'order'
+  | 'time create new'
+  | 'time create old'
+  | 'time update new'
+  | 'time update old'
+
+const orderBy = ref<OrderBy>('order')
+
+const sortedCards = computed(() => {
+  switch (orderBy.value) {
+    case 'order':
+      return store.cards.sort((c1, c2) => c1.order - c2.order)
+    case 'time create new':
+      return store.cards.sort(
+        (c1, c2) => new Date(c2.createdAt).getTime() - new Date(c1.createdAt).getTime()
+      )
+    case 'time create old':
+      return store.cards.sort(
+        (c1, c2) => new Date(c1.createdAt).getTime() - new Date(c2.createdAt).getTime()
+      )
+    case 'time update new':
+      return store.cards.sort(
+        (c1, c2) => new Date(c2.updatedAt).getTime() - new Date(c1.updatedAt).getTime()
+      )
+    case 'time update old':
+      return store.cards.sort(
+        (c1, c2) => new Date(c1.updatedAt).getTime() - new Date(c2.updatedAt).getTime()
+      )
+  }
+})
+
+//
 
 const popupColorPicker = ref<InstanceType<typeof AppPopup> | null>(null)
 
@@ -111,6 +163,8 @@ const pickColor = async (color: string, cardId?: string) => {
   await store.updateOne(cardId, { color })
 }
 
+//
+
 type DraggableChangeEvent<T> = {
   moved: { element: T; oldIndex: number; newIndex: number }
 }
@@ -120,9 +174,7 @@ const changeCardOrder = async (event: DraggableChangeEvent<TodoCard>) => {
   await store.updateOne(card.id, { order: newOrder })
 }
 
-const cardsSortedByOrder = computed(() =>
-  store.cards.sort((card1, card2) => card1.order - card2.order)
-)
+//
 
 const selectedCardId = ref<string>()
 
@@ -132,6 +184,8 @@ const selectCard = (cardId: string) => {
   selectedCardId.value = cardId
   emit('select-card', cardId)
 }
+
+//
 
 const createCard = async (title: string, order?: number) => {
   if (title.trim() === '') return
